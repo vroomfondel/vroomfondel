dec9f4b46e25e57e9bdbbe5e7ead1c39271318b2
Henning Thieß <ht@xomox.cc>
2025-12-09 17:47:57 +0100

    Refactor include.sh to dynamically handle multiple include.local.sh paths
    [skip actions]

--- scripts/include.sh
+++ scripts/include.sh
@@ -3,18 +3,22 @@ GITHUB_USERNAME="vroomfondel"
 
 # echo \$0: $0
 
-include_local_sh="./include.local.sh"
-include_local_sh2="$(dirname "$0")/scripts/include.local.sh"
 
-if [ -e "${include_local_sh}" ] ; then
-  echo "${include_local_sh}" to be read...
-  source "${include_local_sh}"
-else
-  # echo "${include_local_sh}" does not exist...
-  if [ -e "${include_local_sh2}" ] ; then
-    echo "${include_local_sh2}" to be read...
-    source "${include_local_sh2}"
-  else
-    echo NEITHER "${include_local_sh}" NOR "${include_local_sh2}" exist...
+declare -a include_local_sh
+include_local_sh[0]="$(dirname "$0")/include.local.sh"
+include_local_sh[1]="$(dirname "$0")/scripts/include.local.sh"
+include_local_sh[2]="$(dirname "$0")/../scripts/include.local.sh"
+found=false
+
+for path in "${include_local_sh[@]}"; do
+  if [ -e "${path}" ]; then
+    echo "${path} will be read..."
+    source "${path}"
+    found=true
+    break
   fi
+done
+
+if [ "$found" = false ]; then
+  echo "No include.local.sh file[s] found."
 fi
